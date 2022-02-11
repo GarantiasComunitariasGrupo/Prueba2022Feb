8bfac55ec27ab29026396625161bf8c8d61f5d6a
laptop gcg 07 <lapgcg07@gcg.local>
2022-02-11 08:18:17 -0500

    Eliminar typescript

--- public/view/script.js
+++ public/view/script.js
@@ -1,26 +1,9 @@
-/** @template T @typedef { import('../../assets/js/request.js').DataType<T> } DataType */
 import { ApiRequest } from "../../assets/js/request.js";
-
-/**
- * @typedef {object} Caracteristicas Composición del computador
- * @property {string | number} gce_id Idetificación única del computador
- * @property {string} gce_nombre_equipo Nombre del equipo
- * @property {string} gce_board Tipo de placa base
- * @property {string} gce_case Modelo de la torre
- * @property {string} gce_procesador Marca del procesador
- * @property {string} gce_grafica Marca de la trajeta gráfica
- * @property {string} gce_ram Tamaño total de ram
- * @property {string} gce_disco_duro Modelo del disco duro
- * @property {string} gce_teclado Tipo de teclado
- * @property {string} gce_mouse Tipo de mouse
- * @property {string} gce_pantalla Tamaño total de la pantalla
- * @property {string | number} gce_estado Estado del registro
- */
 
 /** Clase que representa al componente computador */
 class Computador {
 
-  /** @type {(keyof Caracteristicas)[]} Listado de campos de la tabla de computadores */
+  /** Listado de campos de la tabla de computadores */
   static columnList = [
     'gce_nombre_equipo',
     'gce_board',
@@ -40,8 +23,8 @@ class Computador {
 
   /** Actualiza el listado de computadores en la tabla */
   static get() {
-    ApiRequest.get('Caracteristicas', 'getAll').then(/** @param {DataType<Caracteristicas[]>} response */response => {
-      /** @type {HTMLElement} Referencia del cuerpo de la tabla */
+    ApiRequest.get('Caracteristicas', 'getAll').then(response => {
+      /** Referencia del cuerpo de la tabla */
       const tbody = document.querySelector('#list-table tbody');
       tbody.innerHTML = ''; // Limpia la tabla
 
@@ -66,28 +49,23 @@ class Computador {
     }).catch(error => console.log('Ha ocurrido un error', error));
   }
 
-  /** 
-   * Registra un computador en la base de datos 
-   * @param {SubmitEvent} event Evento submit del formulario
-   */
+  /** Registra un computador en la base de datos */
   static add = (event) => {
     event.preventDefault(); // Cancela el restablecimiento de la página
 
     /** Formulario de registro */
-    const registerForm =  /** @type {HTMLFormElement} */ (event.target);
+    const registerForm = event.target;
 
-    /** @type {Partial<Record<keyof Caracteristicas, string>>} */
     const parameters = {};
 
     this.columnList.forEach(columnName => {
       if (columnName !== 'gce_id') {
-        /** @type {HTMLInputElement | HTMLSelectElement} */
         const input = registerForm.querySelector(`[name="${columnName}"]`);
         parameters[columnName] = input?.value;
       }
     });
 
-    ApiRequest.post('Caracteristicas', 'addOne', parameters).then(/** @param {DataType<Caracteristicas[]>} response */(response) => {
+    ApiRequest.post('Caracteristicas', 'addOne', parameters).then((response) => {
       console.log('Añadir', response, response.data);
     }).catch(error => console.log('Ha ocurrido un error', error));
   };
